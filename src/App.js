1c5973a20719a7148cb33e9e2e6ecacd6e01c7ba
dongkyun2331 <dongkyun2331@gmail.com>
2023-03-07 09:45:59 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>Note</h1>
+        <button>글쓰기</button>
       </header>
+      <ul className="list">
+        <li className="note">
+          <h3>리엑트 콤퍼넌트</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet ex
+            eligendi deserunt quod odio! Beatae provident a earum illum autem
+            non quibusdam eum eius asperiores nemo! Sint dicta accusantium
+            mollitia.
+          </p>
+          <div className="bottom">
+            <div className="date">2023-03-12</div>
+            <button>수정</button>
+            <button>삭제</button>
+          </div>
+        </li>
+        <li className="note">
+          <h3>리엑트 콤퍼넌트</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet ex
+            eligendi deserunt quod odio! Beatae provident a earum illum autem
+            non quibusdam eum eius asperiores nemo! Sint dicta accusantium
+            mollitia.
+          </p>
+          <div className="bottom">
+            <div className="date">2023-03-12</div>
+            <button>수정</button>
+            <button>삭제</button>
+          </div>
+        </li>
+        <li className="note">
+          <h3>리엑트 콤퍼넌트</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet ex
+            eligendi deserunt quod odio! Beatae provident a earum illum autem
+            non quibusdam eum eius asperiores nemo! Sint dicta accusantium
+            mollitia.
+          </p>
+          <div className="bottom">
+            <div className="date">2023-03-12</div>
+            <button>수정</button>
+            <button>삭제</button>
+          </div>
+        </li>
+        <li className="note">
+          <h3>리엑트 콤퍼넌트</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet ex
+            eligendi deserunt quod odio! Beatae provident a earum illum autem
+            non quibusdam eum eius asperiores nemo! Sint dicta accusantium
+            mollitia.
+          </p>
+          <div className="bottom">
+            <div className="date">2023-03-12</div>
+            <button>수정</button>
+            <button>삭제</button>
+          </div>
+        </li>
+      </ul>
     </div>
-  );
+  )
 }
-
-export default App;
