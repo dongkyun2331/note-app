c50a2753877f835544f5b6abc286865901bc0dbb
dongkyun2331 <dongkyun2331@gmail.com>
2023-03-07 10:11:15 +0900

    ㅁ

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1>👩Note</h1>
+      <button>글쓰기</button>
+    </header>
+  )
+}
