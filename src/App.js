210bbacad210aa3cf35cb795d8301a31dcab618c
dongkyun2331 <dongkyun2331@gmail.com>
2023-03-07 10:11:58 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,70 +1,12 @@
 import React from 'react'
+import Header from './Header'
+import List from './List'
 
 export default function App() {
   return (
     <div className="App">
-      <header className="header">
-        <h1>Note</h1>
-        <button>글쓰기</button>
-      </header>
-      <ul className="list">
-        <li className="note">
-          <h3>리엑트 콤퍼넌트</h3>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet ex
-            eligendi deserunt quod odio! Beatae provident a earum illum autem
-            non quibusdam eum eius asperiores nemo! Sint dicta accusantium
-            mollitia.
-          </p>
-          <div className="bottom">
-            <div className="date">2023-03-12</div>
-            <button>수정</button>
-            <button>삭제</button>
-          </div>
-        </li>
-        <li className="note">
-          <h3>리엑트 콤퍼넌트</h3>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet ex
-            eligendi deserunt quod odio! Beatae provident a earum illum autem
-            non quibusdam eum eius asperiores nemo! Sint dicta accusantium
-            mollitia.
-          </p>
-          <div className="bottom">
-            <div className="date">2023-03-12</div>
-            <button>수정</button>
-            <button>삭제</button>
-          </div>
-        </li>
-        <li className="note">
-          <h3>리엑트 콤퍼넌트</h3>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet ex
-            eligendi deserunt quod odio! Beatae provident a earum illum autem
-            non quibusdam eum eius asperiores nemo! Sint dicta accusantium
-            mollitia.
-          </p>
-          <div className="bottom">
-            <div className="date">2023-03-12</div>
-            <button>수정</button>
-            <button>삭제</button>
-          </div>
-        </li>
-        <li className="note">
-          <h3>리엑트 콤퍼넌트</h3>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet ex
-            eligendi deserunt quod odio! Beatae provident a earum illum autem
-            non quibusdam eum eius asperiores nemo! Sint dicta accusantium
-            mollitia.
-          </p>
-          <div className="bottom">
-            <div className="date">2023-03-12</div>
-            <button>수정</button>
-            <button>삭제</button>
-          </div>
-        </li>
-      </ul>
+      <Header></Header>
+      <List></List>
     </div>
   )
 }
---
+++ src/List.js
@@ -0,0 +1,60 @@
+import React from 'react'
+
+export default function List() {
+  return (
+    <ul className="list">
+      <li className="note">
+        <h3>리엑트 콤퍼넌트</h3>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet ex
+          eligendi deserunt quod odio! Beatae provident a earum illum autem non
+          quibusdam eum eius asperiores nemo! Sint dicta accusantium mollitia.
+        </p>
+        <div className="bottom">
+          <div className="date">2023-03-12</div>
+          <button>수정</button>
+          <button>삭제</button>
+        </div>
+      </li>
+      <li className="note">
+        <h3>리엑트 콤퍼넌트</h3>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet ex
+          eligendi deserunt quod odio! Beatae provident a earum illum autem non
+          quibusdam eum eius asperiores nemo! Sint dicta accusantium mollitia.
+        </p>
+        <div className="bottom">
+          <div className="date">2023-03-12</div>
+          <button>수정</button>
+          <button>삭제</button>
+        </div>
+      </li>
+      <li className="note">
+        <h3>리엑트 콤퍼넌트</h3>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet ex
+          eligendi deserunt quod odio! Beatae provident a earum illum autem non
+          quibusdam eum eius asperiores nemo! Sint dicta accusantium mollitia.
+        </p>
+        <div className="bottom">
+          <div className="date">2023-03-12</div>
+          <button>수정</button>
+          <button>삭제</button>
+        </div>
+      </li>
+      <li className="note">
+        <h3>리엑트 콤퍼넌트</h3>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet ex
+          eligendi deserunt quod odio! Beatae provident a earum illum autem non
+          quibusdam eum eius asperiores nemo! Sint dicta accusantium mollitia.
+        </p>
+        <div className="bottom">
+          <div className="date">2023-03-12</div>
+          <button>수정</button>
+          <button>삭제</button>
+        </div>
+      </li>
+    </ul>
+  )
+}
